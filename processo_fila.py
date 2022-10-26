0b3bc303845751b69549f9e595983f253c1bdccf
cesarvasco2 <70148129+cesarvasco2@users.noreply.github.com>
2022-10-26 16:36:49 -0300

    Update processo_fila.py

--- processo_fila.py
+++ processo_fila.py
@@ -12,7 +12,6 @@ tabela = dynamodb.Table('log_sideccr')
 sqs = boto3.resource('sqs', region_name='us-east-1')
 queue = sqs.get_queue_by_name(QueueName='sideccr')
 #Loop
-erros = 0
 print('PROCESSO INICIADO...' )
 while True:
     resp_http = False
@@ -37,23 +36,28 @@ while True:
         #Montar o log para o banco de dados G-Hidro
                 payload_dict['resposta_sideccr'] = {'mensagem_erro':resp_http_msg, 'codigo_erro':resp_http }
                 payload_dict['timestamp'] = payload_dict['data_hora_utlizado']
+                payload_dict['medidor'] =  payload_dict['dados_sideccr']['codigo_medidor']
                 payload_dict['vazao'] = str(round(payload_dict['vazao']/3600, 3))
                 Operations.create(tabela,payload_dict)
                 #print(payload_dict)
         # Apaga mensagem da fila
-                if  r.status_code == 200 and resp_http =='000' :
-                         message.delete()
+                if  r.status_code == 200:
+                        message.delete()
+                else:
+                    time.sleep(60)
                 # Print no console em caso de erro
                 if  resp_http != '000' or r.status_code != 200:
                         if not resp_http == False:
-                                print('\n')
-                if  resp_http == '006' or resp_http =='007':
-                        erros = erros + 1
-                        print('Erro registrando valor\n Quantidade de erros registrados desde o inicio do processo:'+ erros)
-                        message.delete()                                       
+                                print('[STATUS] ERRO SIDECCR')
+                '''if  resp_http == '006' or resp_http =='007':
+                        print('Erro registrando valor')
+                        message.delete() '''                                      
                 print('{}\nCódigo HTTP: {}\nResposta do servidor: {}\nTimestamp: {}'.format(r.url, r.status_code, resp_http_msg, data_hora))
         time.sleep(2)
     except:
-        print('[STATUS] Ocorreu um erro esperando 15m para tentar novamente')
-        time.sleep(900)
+        if payload_dict['vazao'] < 0:
+                message.delete() 
+                print('Vazão negativa'+ ','+dict_url['usuario']+ ','+dict_url['medidor']+ ','+dict_url['chave'])
+        print('[STATUS] Ocorreu um erro esperando 5m para tentar novamente')
+        time.sleep(300)
                       
